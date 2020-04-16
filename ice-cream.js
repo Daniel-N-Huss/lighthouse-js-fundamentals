456e4f589625ca3adeb744dda0385434ff7c7746
Daniel-N-Huss <daniel.n.huss@gmail.com>
2020-04-16 05:33:55 +0000

    Recommiting banana bread ingredient loops

--- ice-cream.js
+++ ice-cream.js
@@ -4,3 +4,9 @@ console.log(iceCreamFlavours);
 iceCreamFlavours.push('root beer');
 
 console.log(iceCreamFlavours);
+
+console.log(iceCreamFlavours[0]);
+
+console.log(iceCreamFlavours[4]);
+
+console.log(iceCreamFlavours.length);
